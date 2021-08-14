92e1180012297f5cfb6eccb9687a209dec9a5058
mjbiener <michaelbiener@yahoo.com>
2021-08-14 12:50:56 -0400

    updated  album list page contents

--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Album, Track
 
 
-
 def album_list(request):
     albums = Album.objects.all()
     return render(request, 'core/albums.html', {'albums': albums})
